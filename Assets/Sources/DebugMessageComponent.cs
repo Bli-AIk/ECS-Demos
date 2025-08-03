d99eafb773dd605a56973142db0a010696f4e4bd
Bli_Aik <haikun2333@126.com>
2025-08-03 10:02:09 +0800

    change Entitas version to 1.13.0

---
+++ Assets/Sources/DebugMessageComponent.cs
@@ -0,0 +1,10 @@
+using Entitas;
+
+namespace Sources
+{
+    [Game]
+    public class DebugMessageComponent : IComponent 
+    {    
+        public string Message;
+    }
+}
--- readme.md
+++ readme.md
@@ -4,3 +4,6 @@
 ## 计划
 - Hello world
 - 贪吃蛇
+
+## 记录
+- 项目用的是 Entitas 1.13.0；1.14.X Generated生成不全。1.13目前没啥问题
